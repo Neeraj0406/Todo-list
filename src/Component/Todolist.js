f2820d83ade9389d204d1fed19c16f9edda026f9
Neeraj0406 <neerajsingh0406@gmail.com>
2022-01-20 13:58:12 +0530

    minor changes

--- src/Component/Todolist.js
+++ src/Component/Todolist.js
@@ -1,122 +1,136 @@
-import React,{useState,useEffect} from 'react'
-import todolistimage from "../images/todolist.png"
-import "./todolist.css"
+import React, { useState, useEffect } from "react";
+import todolistimage from "../images/todolist.png";
+import "./todolist.css";
 
-const getLocalItems = () =>{
-    let list = localStorage.getItem("Items");
-    if(list){
-        return JSON.parse(localStorage.getItem("Items"));
-    }
-    else{
-        return []
-    }
-}
+const getLocalItems = () => {
+  let list = localStorage.getItem("Items");
+  if (list) {
+    return JSON.parse(localStorage.getItem("Items"));
+  } else {
+    return [];
+  }
+};
 const Todolist = () => {
+  const [inputData, setInputData] = useState("");
+  const [items, setItems] = useState(getLocalItems());
+  const [updateToggle, setUpdateToggle] = useState(false);
+  const [isEditItem, setIsEditItem] = useState();
 
-    const [inputData, setInputData] = useState("")
-    const [items, setItems] = useState(getLocalItems());
-    const [updateToggle , setUpdateToggle] = useState(false)
-    const [isEditItem , setIsEditItem] = useState();
-    
-    
-    const showList = () =>{
-        if(!inputData){
-            alert("please enter data");
-        }else if(inputData && updateToggle){
-            setItems(
-                items.map((elem) =>{
-                    if(elem.id === isEditItem)
-                    {
-                        return {...elem , name:inputData}
-                    }
-                    return elem;
-                })
-                )
-                setUpdateToggle(false)
-                setInputData("");
-                setIsEditItem(null);
-        }
-        else{
-            const allInputData = {id: new Date().getTime().toString() , name : inputData}
-         
-            setItems([...items, allInputData])
-            setInputData("")
-        }
-    }
-var newList = []
-    const deleteItems = (id) =>{
-         newList = items.filter((elem) =>{
-            return elem.id!=id;
+  const showList = () => {
+    if (!inputData) {
+      alert("please enter data");
+    } else if (inputData && updateToggle) {
+      setItems(
+        items.map((elem) => {
+          if (elem.id === isEditItem) {
+            return { ...elem, name: inputData };
+          }
+          return elem;
         })
-        setItems(newList)
+      );
+      setUpdateToggle(false);
+      setInputData("");
+      setIsEditItem(null);
+    } else {
+      const allInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      };
+
+      setItems([...items, allInputData]);
+      setInputData("");
     }
+  };
+  var newList = [];
+  const deleteItems = (id) => {
+    newList = items.filter((elem) => {
+      return elem.id != id;
+    });
+    setItems(newList);
+  };
 
-    const RemoveAll = () => {
-        setItems([])
-    }
-    
-    const updateItems = (id) => {
-        
-        let newEditItem = items.find((elem) =>{
-            return id === elem.id;
-        })
-        console.log(newEditItem.name);
-        setUpdateToggle(true);
-        setInputData(newEditItem.name);
-        setIsEditItem(id);
-        
-    }
+  const RemoveAll = () => {
+    setItems([]);
+  };
 
-    //  add data to local storage
-    useEffect(() => {
-       localStorage.setItem("Items" , JSON.stringify(items))
-    }, [items])
+  const updateItems = (id) => {
+    let newEditItem = items.find((elem) => {
+      return id === elem.id;
+    });
+    console.log(newEditItem.name);
+    setUpdateToggle(true);
+    setInputData(newEditItem.name);
+    setIsEditItem(id);
+  };
 
-    return (
-        <>
-            <div className="main_container">
-                <div className="child_container">
-                    <figure>
-                        <img src={todolistimage} alt="todolist image" />
-                        <figcaption className="fig_caption"> Add your list here ✌ </figcaption>
-                    </figure>
+  //  add data to local storage
+  useEffect(() => {
+    localStorage.setItem("Items", JSON.stringify(items));
+  }, [items]);
 
-                    
-                    <div className="item_input_div">
-                        <input type="text" className="item_input" placeholder="✍ Add items..."
-                            onChange={(e)=> setInputData(e.target.value)}
-                            value={inputData}
-                        /> 
-                        {
-                            updateToggle ? <i className="far fa-edit input_btn" title="Add Item" onClick={showList}/> : <i className="fas fa-plus input_btn" title="Add Item" onClick={showList}/>
-                        }
-                        
-                        
-                    </div>
+  return (
+    <>
+      <div className="main_container">
+        <div className="child_container">
+          <figure>
+            <img src={todolistimage} alt="todolist image" />
+            <figcaption className="fig_caption">
+              {" "}
+              Add your list here ✌{" "}
+            </figcaption>
+          </figure>
 
-                    <div className="each_item_container">
+          <div className="item_input_div">
+            <input
+              type="text"
+              className="item_input"
+              placeholder="✍ Add items..."
+              onChange={(e) => setInputData(e.target.value)}
+              value={inputData}
+            />
+            {updateToggle ? (
+              <i
+                className="far fa-edit input_btn"
+                title="Add Item"
+                onClick={showList}
+              />
+            ) : (
+              <i
+                className="fas fa-plus input_btn"
+                title="Add Item"
+                onClick={showList}
+              />
+            )}
+          </div>
 
-                        {items.map((elem)=> {
-                            return(
-                                <div className="list_div" key={elem.id}>
-                                    <h3 className="each_item"  > {elem.name} </h3>
-                                    <i className="fas fa-times " title="Delete Item" onClick={() => deleteItems(elem.id)}/>
-                                    <i className="far fa-edit" title="Update Item" onClick={() => updateItems(elem.id)}/>
-                                    
-                                </div>
-                                )
-                        })}
-
-                        <button className="remove_btn" onClick={RemoveAll}> Remove All</button>
-                        
-                    </div>
+          <div className="each_item_container">
+            {items.map((elem) => {
+              return (
+                <div className="list_div" key={elem.id}>
+                  <h3 className="each_item"> {elem.name} </h3>
+                  <i
+                    className="fas fa-times "
+                    title="Delete Item"
+                    onClick={() => deleteItems(elem.id)}
+                  />
+                  <i
+                    className="far fa-edit"
+                    title="Update Item"
+                    onClick={() => updateItems(elem.id)}
+                  />
                 </div>
-            </div>
-        </>
-    )
-}
+              );
+            })}
 
-export default Todolist
+            <button className="remove_btn" onClick={RemoveAll}>
+              {" "}
+              Remove All
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-
-
+export default Todolist;
